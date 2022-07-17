07c2f0ace7e3b4335eb0613aaf732e57f592d306
cparragatorres <humbertpt14@gmail.com>
2022-07-17 02:34:59 -0500

    Slider 5

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,76 +9,80 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //*1er modo
-    // return Padding(
-    //   padding: const EdgeInsets.all(12.0),
-    //   child: Column(
-    //     crossAxisAlignment: CrossAxisAlignment.center,
-    //     children: [
-    //       divider20,
-    //       const Text(
-    //         "Museum Explorer",
-    //         style: TextStyle(
-    //           color: Colors.white,
-    //           fontSize: 20.0,
-    //         ),
-    //       ),
-    //       divider12,
-    //       const Text(
-    //         "What do you want to visit today",
-    //         style: TextStyle(
-    //           color: Colors.white54,
-    //           fontSize: 14.0,
-    //         ),
-    //       ),
-    //       SingleChildScrollView(
-    //         physics: const BouncingScrollPhysics(),
-    //         scrollDirection: Axis.horizontal,
-    //         child: Row(
-    //             children: imagesDummy
-    //                 .map((e) => ItemSliderWidget(image: e))
-    //                 .toList()),
-    //       ),
-    //       divider6,
-    //       ElevatedButton(
-    //         style: ElevatedButton.styleFrom(
-    //           primary: Colors.blueAccent,
-    //         ),
-    //         onPressed: () {},
-    //         child: Container(
-    //           margin: const EdgeInsets.symmetric(horizontal: 10.0),
-    //           child: const Text(
-    //             "Explora +3000 colecciones",
-    //           ),
-    //         ),
-    //       ),
-    //     ],
-    //   ),
-    // );
-    //*2do modo
-    return Container(
-      height: 200.0,
-      child: PageView(
-        scrollDirection: Axis.vertical,
-        physics: const BouncingScrollPhysics(),
-        onPageChanged: (int value) {
-          print(value);
-        },
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            color: Colors.red,
+          divider20,
+          const Text(
+            "Museum Explorer",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
           ),
-          Container(
-            color: Colors.purple,
+          divider12,
+          const Text(
+            "What do you want to visit today",
+            style: TextStyle(
+              color: Colors.white54,
+              fontSize: 14.0,
+            ),
           ),
-          Container(
-            color: Colors.green,
+          // *METODO 1
+          // SingleChildScrollView(
+          //   physics: const BouncingScrollPhysics(),
+          //   scrollDirection: Axis.horizontal,
+          //   child: Row(
+          //       children: imagesDummy
+          //           .map((e) => ItemSliderWidget(image: e))
+          //           .toList()),
+          // ),
+          // divider6,
+          // *METODO  2
+          SizedBox(
+            height: 180.0,
+            child: PageView.builder(
+              controller: PageController(
+                viewportFraction: 0.47,
+                initialPage: 1,
+              ),
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              padEnds: false,
+              pageSnapping: true,
+              onPageChanged: (int value) {
+                print(value);
+              },
+              itemCount: imagesDummy.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: const EdgeInsets.all(6.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white10,
+                    borderRadius: BorderRadius.circular(20.0),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(imagesDummy[index]),
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
-          Container(
-            color: Colors.blue,
-          ),
-          Container(
-            color: Colors.orange,
+          divider6,
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.blueAccent,
+            ),
+            onPressed: () {},
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: const Text(
+                "Explora +3000 colecciones",
+              ),
+            ),
           ),
         ],
       ),
