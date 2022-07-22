7918d4a5a69e9c21eb20d6ee94a265ef0a421b1c
cparragatorres <humbertpt14@gmail.com>
2022-07-22 04:09:01 -0500

    Selecciones 5

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 import 'package:museumapp/helpers/data/data_dummy.dart';
+import 'package:museumapp/ui/general/colors.dart';
 import 'package:museumapp/ui/general/general_widget.dart';
 
 import '../ui/widgets/item_slider_1_widget.dart';
@@ -10,186 +11,272 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
     return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            divider20,
-            const Text(
-              "Museum Explorer",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-              ),
-            ),
-            divider12,
-            const Text(
-              "What do you want to visit today",
-              style: TextStyle(
-                color: Colors.white54,
-                fontSize: 14.0,
-              ),
-            ),
-            // *METODO 1
-            // SingleChildScrollView(
-            //   physics: const BouncingScrollPhysics(),
-            //   scrollDirection: Axis.horizontal,
-            //   child: Row(
-            //       children: imagesDummy
-            //           .map((e) => ItemSliderWidget(image: e))
-            //           .toList()),
-            // ),
-            // divider6,
-            // *METODO  2
-            SizedBox(
-              height: 180.0,
-              child: PageView.builder(
-                controller: PageController(
-                  viewportFraction: 0.47,
-                  initialPage: 1,
-                ),
-                scrollDirection: Axis.horizontal,
-                physics: const BouncingScrollPhysics(),
-                padEnds: false,
-                pageSnapping: true,
-                onPageChanged: (int value) {
-                  print(value);
-                },
-                itemCount: imagesDummy.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: const EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white10,
-                      borderRadius: BorderRadius.circular(20.0),
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(imagesDummy[index]),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            divider6,
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blueAccent,
-              ),
-              onPressed: () {},
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: const Text(
-                  "Explora +3000 colecciones",
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              divider20,
+              const Text(
+                "Museum Explorer",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
                 ),
               ),
-            ),
-            divider30,
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 22.0, horizontal: 14.0),
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.06),
-                borderRadius: BorderRadius.circular(12.0),
+              divider12,
+              const Text(
+                "What do you want to visit today",
+                style: TextStyle(
+                  color: Colors.white54,
+                  fontSize: 14.0,
+                ),
               ),
-              child: Column(
-                children: [
-                  Image.asset(
-                    'assets/images/image1.png',
-                    height: 180.0,
+              // *METODO 1
+              // SingleChildScrollView(
+              //   physics: const BouncingScrollPhysics(),
+              //   scrollDirection: Axis.horizontal,
+              //   child: Row(
+              //       children: imagesDummy
+              //           .map((e) => ItemSliderWidget(image: e))
+              //           .toList()),
+              // ),
+              // divider6,
+              // *METODO  2
+              SizedBox(
+                height: 180.0,
+                child: PageView.builder(
+                  controller: PageController(
+                    viewportFraction: 0.47,
+                    initialPage: 1,
                   ),
-                  const Text(
-                    "Culture Box",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 28.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const Text(
-                    "Suscribete para recibir noticias, historias y actualizaciones semanalmente",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white70,
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextButton(
-                        style: TextButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(horizontal: 22.0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(6.0),
-                            side: BorderSide(
-                              width: 1.2,
-                              color: Colors.white10,
-                            ),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          "No, Gracias",
-                        ),
-                      ),
-                      dividerWidth12,
-                      TextButton(
-                        style: TextButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(horizontal: 22.0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(6.0),
-                            side: BorderSide(
-                              width: 1.2,
-                              color: Colors.white10,
-                            ),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          "Suscribirme",
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            divider30,
-            const Text(
-              "las mejores selecciones de hoy",
-              style: TextStyle(color: Colors.white, fontSize: 20.0),
-            ),
-            divider20,
-            Container(
-                height: 300.0,
-                child: Swiper(
-                  viewportFraction: 0.6,
-                  scale: 0.9,
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
+                  padEnds: false,
+                  pageSnapping: true,
+                  onPageChanged: (int value) {
+                    print(value);
+                  },
+                  itemCount: imagesDummy.length,
                   itemBuilder: (BuildContext context, int index) {
                     return Container(
-                      width: 200.0,
+                      margin: const EdgeInsets.all(6.0),
                       decoration: BoxDecoration(
+                        color: Colors.white10,
+                        borderRadius: BorderRadius.circular(20.0),
                         image: DecorationImage(
                           fit: BoxFit.cover,
-                          image: NetworkImage(
-                            "https://images.pexels.com/photos/12383337/pexels-photo-12383337.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-                          ),
+                          image: NetworkImage(imagesDummy[index]),
                         ),
                       ),
                     );
                   },
-                  itemCount: 10,
-                  itemWidth: 240,
-                  layout: SwiperLayout.STACK,
-                  autoplay: true,
                 ),
+              ),
+              divider6,
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blueAccent,
+                ),
+                onPressed: () {},
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: const Text(
+                    "Explora +3000 colecciones",
+                  ),
+                ),
+              ),
+              divider30,
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 22.0, horizontal: 14.0),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.06),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/image1.png',
+                      height: 180.0,
+                    ),
+                    const Text(
+                      "Culture Box",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      "Suscribete para recibir noticias, historias y actualizaciones semanalmente",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(horizontal: 22.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(6.0),
+                              side: BorderSide(
+                                width: 1.2,
+                                color: Colors.white10,
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            "No, Gracias",
+                          ),
+                        ),
+                        dividerWidth12,
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(horizontal: 22.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(6.0),
+                              side: BorderSide(
+                                width: 1.2,
+                                color: Colors.white10,
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            "Suscribirme",
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              divider30,
+              const Text(
+                "las mejores selecciones de hoy",
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+              divider20,
+              SizedBox(
+                height: 400.0,
+                child: Swiper(
+                  // viewportFraction: 0.6,
+                  // scale: 0.9,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      width: 200.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.0),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            imagesDummy[index],
+                          ),
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.center,
+                                colors: [
+                                  kBrandPrimaryColor.withOpacity(0.8),
+                                  kBrandPrimaryColor.withOpacity(0.1),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  "Lorem ipsum dolor sit amet",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                divider6,
+                                Text(
+                                  "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    color: Colors.white70,
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: imagesDummy.length,
+                  itemWidth: width * 0.7,
+                  layout: SwiperLayout.STACK,
+                  autoplay: false,
+                  controller: SwiperController(),
+                ),
+              ),
+              divider30,
+            ],
+          ),
+          ),
+          Container(
+            height: 300.0,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                  "https://images.pexels.com/photos/1194420/pexels-photo-1194420.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                ),
+              )
             ),
-            divider40,
-          ],
-        ),
+            child: Column(
+              children: const[
+                Text(
+                  "El Festival Cultural",
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  "Encabezando hoy...",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+
+          ),
+          divider40,
+          divider40,
+        ],
       ),
     );
   }
